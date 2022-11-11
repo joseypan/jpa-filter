69216e152ee1c0473c85cbb95599f0188ce3e068
panxumin <joseypan@163.com>
2022-11-11 17:23:41 +0800

    feat: 增加validWhen测试用例

--- test/getFilterParams.spec.ts
+++ test/getFilterParams.spec.ts
@@ -1,33 +1,39 @@
+import { IFilterMap } from "./../src/index";
 import { getFilterParams } from "../src/index";
 import { describe, test, expect } from "vitest";
 import { FilterComparatorEnum, LogicOperatorEnum } from "../src/@types";
 describe("getFilterParams", () => {
   test("test getFilterParams basic use", () => {
     const basicQueryInfo = {
-      workerType: "workerType",
-      deviceName: "deviceName1",
+      deviceName: "deviceNo1",
+      age: 12,
+      isValid: false,
+      date: new Date("2022-10-30 00:00:00"),
     };
     const filterMap = [
       {
-        operator: LogicOperatorEnum.OR, //"OR"
-        targetField: "",
+        operator: FilterComparatorEnum.CONTAINS,
+        targetField: "deviceName",
         validWhen: true,
-        children: [
-          {
-            operator: FilterComparatorEnum.EQ,
-            targetField: "workerType",
-            validWhen: true,
-            children: null,
-            directValue: basicQueryInfo.workerType,
-          },
-          {
-            operator: FilterComparatorEnum.CONTAINS,
-            targetField: "deviceName",
-            validWhen: true,
-            children: null,
-            directValue: basicQueryInfo.deviceName,
-          },
-        ],
+        children: null,
+      },
+      {
+        operator: FilterComparatorEnum.EQ,
+        targetField: "age",
+        validWhen: true,
+        children: null,
+      },
+      {
+        operator: FilterComparatorEnum.EQ,
+        targetField: "isValid",
+        validWhen: true,
+        children: null,
+      },
+      {
+        operator: FilterComparatorEnum.LE,
+        targetField: "date",
+        validWhen: true,
+        children: null,
       },
     ];
     const resultData = {
@@ -35,29 +41,84 @@ describe("getFilterParams", () => {
       "@classType": "logic",
       filters: [
         {
-          logicOperator: LogicOperatorEnum.OR,
-          "@classType": "logic",
-          filters: [
-            {
-              "@classType": "comparator",
-              fieldName: "workerType",
-              filterComparator: FilterComparatorEnum.EQ,
-              ignoreCase: true,
-              params: ["workerType"],
-            },
-            {
-              "@classType": "comparator",
-              fieldName: "deviceName",
-              filterComparator: FilterComparatorEnum.CONTAINS,
-              ignoreCase: true,
-              params: ["deviceName1"],
-            },
-          ],
+          "@classType": "comparator",
+          fieldName: "deviceName",
+          filterComparator: FilterComparatorEnum.CONTAINS,
+          ignoreCase: true,
+          params: ["deviceNo1"],
+        },
+        {
+          "@classType": "comparator",
+          fieldName: "age",
+          filterComparator: FilterComparatorEnum.EQ,
+          ignoreCase: true,
+          params: ["12"],
+        },
+        {
+          "@classType": "comparator",
+          fieldName: "isValid",
+          filterComparator: FilterComparatorEnum.EQ,
+          ignoreCase: true,
+          params: ["false"],
+        },
+        {
+          "@classType": "comparator",
+          fieldName: "date",
+          filterComparator: FilterComparatorEnum.LE,
+          ignoreCase: true,
+          params: ["2022-10-30 00:00:00"],
         },
       ],
     };
     expect(
       getFilterParams(LogicOperatorEnum.AND, basicQueryInfo, filterMap)
     ).toEqual(resultData);
+  });
+
+  test("validWhen field supports expression", () => {
+    const queryForm = {
+      name: "josey",
+    };
+    const filterMap: IFilterMap[] = [
+      {
+        operator: FilterComparatorEnum.EQ,
+        targetField: "name",
+        validWhen: (data) => {
+          return data.name === "josey";
+        },
+      },
+    ];
+    const resultData = {
+      logicOperator: LogicOperatorEnum.AND,
+      "@classType": "logic",
+      filters: [
+        {
+          "@classType": "comparator",
+          fieldName: "name",
+          filterComparator: FilterComparatorEnum.EQ,
+          ignoreCase: true,
+          params: ["josey"],
+        },
+      ],
+    };
+    expect(
+      getFilterParams(LogicOperatorEnum.AND, queryForm, filterMap)
+    ).toEqual(resultData);
+  });
+
+  test("remove item when validWhen equal false", () => {
+    const queryForm = {
+      name: "josey",
+    };
+    const filterMap: IFilterMap[] = [
+      {
+        operator: FilterComparatorEnum.EQ,
+        targetField: "name",
+        validWhen: false,
+      },
+    ];
+    expect(
+      getFilterParams(LogicOperatorEnum.AND, queryForm, filterMap)
+    ).toEqual(null);
   });
 });
